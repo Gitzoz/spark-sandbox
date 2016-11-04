0f93676b35cb6a5244c7f3d182179de6050b2c50
Stefan Röhrbein <roehrbein@dial.de>
2016-11-04 11:56:34 +0100

    copied the DicisionTreeClassification code snippet from the spark guide with a DataFrame as base data format

--- src/main/scala/de/gitzoz/spark/sandbox/DecisionTreeClassificationExample.scala
+++ src/main/scala/de/gitzoz/spark/sandbox/DecisionTreeClassificationExample.scala
@@ -1,13 +1,89 @@
+/*
+ * Copyright 2016 Stefan Roehrbein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.gitzoz.spark.sandbox
 
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.annotation.Experimental
+import org.apache.spark.annotation.Since
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.IndexToString
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.sql.SparkSession
 
 object DecisionTreeClassificationExample {
-  def main(args: Array[String]):Unit = {
-    val spark = SparkSession.
-    val data = spark.read.format("libsvm").load("spot_nur_halbstreuwinkel_libsvm.txt")
-    
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .appName("DecisionTreeClassificationExample")
+      .getOrCreate()
+
+    val data =
+      spark.read.format("libsvm").load("spot_nur_halbstreuwinkel_libsvm.txt")
+
+    val labelIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      .fit(data)
+
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      .setMaxCategories(4)
+      .fit(data)
+
+    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+    val descisionTreeClassifier = new DecisionTreeClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("indexedFeatures")
+
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(labelIndexer.labels)
+
+    val pipeline = new Pipeline().setStages(
+      Array(labelIndexer,
+            featureIndexer,
+            descisionTreeClassifier,
+            labelConverter))
+
+    val model = pipeline.fit(trainingData)
+    println("Resulting Model after training: " + model.toString())
+
+    val predictions = model.transform(testData)
+
+    predictions.select("predictedLabel", "label", "features").show(5)
+    predictions.show(5)
+
+    val evaluator = new BinaryClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setRawPredictionCol("rawPrediction")
+
+    val accuracy = evaluator.evaluate(predictions)
+
+    println("Test Error =" + (1.0 - accuracy))
+
+    val treeModel =
+      model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+    println("Learned classification tree model:\n" + treeModel.toDebugString)
+
   }
-  
-  
+
 }
