365f05e385b54330845ed343b8d3bb9f9fb4a020
Stefan Röhrbein <roehrbein@dial.de>
2016-11-09 14:53:54 +0100

    playing around with the BisectingKMeans algorithm

--- src/main/scala/de/gitzoz/spark/sandbox/BisectingKMeansExample.scala
+++ src/main/scala/de/gitzoz/spark/sandbox/BisectingKMeansExample.scala
@@ -1,33 +1,89 @@
+/*
+ * Copyright 2016 Stefan Roehrbein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.gitzoz.spark.sandbox
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.clustering.BisectingKMeans
 import org.apache.spark.ml.linalg.Vector
+import java.io.File
+import org.apache.spark.ml.clustering.KMeans
 
 object BisectingKMeansExample {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
-      .appName("BisectingKMeansExample")
-      .getOrCreate()
+    val spark =
+      SparkSession.builder.appName("BisectingKMeansExample").getOrCreate()
 
     val dataset = spark.read.format("libsvm").load("sample_kmeans_data.txt")
+    constructDataForElbowMethod(dataset, spark)
+    ouputPredictionsForLabels(3, dataset, spark)
+  }
 
-    val result = (1 to 10).foldLeft(Map[Int, (Double, Array[Vector])]()) { (acc, idx) =>
-      val bkm = new BisectingKMeans().setK(idx).setSeed(1)
-      val model = bkm.fit(dataset)
+  def constructDataForElbowMethod(dataset: DataFrame, spark: SparkSession) = {
+    val result =
+      (2 to 10).foldLeft(Map[Int, (Double, Array[Vector])]()) { (acc, idx) =>
+        val bkm   = new BisectingKMeans().setK(idx).setSeed(1)
+        val model = bkm.fit(dataset)
 
-      val cost = model.computeCost(dataset)
-      println(s"Within Set Sum of Squared Errors = $cost")
+        val cost = model.computeCost(dataset)
+        println(s"Within Set Sum of Squared Errors = $cost")
 
-      val centers = model.clusterCenters
+        val centers = model.clusterCenters
 
-      acc ++ Map(idx -> (cost, centers))
+        acc ++ Map(idx -> (cost, centers))
+      }
+    writeCostCsv(result)
+  }
+
+  def ouputPredictionsForLabels(clusters: Int,
+                                dataset: DataFrame,
+                                spark: SparkSession) = {
+    val bkm   = new BisectingKMeans().setK(clusters).setSeed(1)
+    val model = bkm.fit(dataset)
+
+    val predictions = model.transform(dataset)
+    writePredictionCsv(predictions)
+  }
+
+  def writeCostCsv(modelResult: Map[Int, (Double, Array[Vector])]) = {
+    val costString = modelResult.toSeq.sortBy(_._1).map {
+      case (idx, (cost, _)) =>
+        s"$idx;$cost\n".replace(".", ",")
     }
-
-    result.foreach {
-      case (idx, (cost, centers)) =>
-        println(s"For $idx Clusters, sum of Squared Errors = $cost")
-        centers.foreach(println)
-    }
+    val stringBuilder = new StringBuilder()
+    costString.foreach { stringBuilder.append(_) }
+    import java.nio.file.{ Paths, Files }
+    import java.nio.charset.StandardCharsets
+    Files.write(Paths.get(s"cost.csv"),
+                stringBuilder.toString.getBytes(StandardCharsets.UTF_8))
+  }
+  def writePredictionCsv(predictions: DataFrame) = {
+    val predictionStrings =
+      predictions.select("label", "prediction").sort("label").collect.map {
+        row =>
+          val label      = row.getAs[Double]("label")
+          val prediction = row.getAs[Int]("prediction")
+          s"$label;$prediction\n".replace(".", ",")
+      }
+    val stringBuilder = new StringBuilder()
+    predictionStrings.foreach { stringBuilder.append(_) }
+    import java.nio.file.{ Paths, Files }
+    import java.nio.charset.StandardCharsets
+    Files.write(Paths.get(s"predictions.csv"),
+                stringBuilder.toString.getBytes(StandardCharsets.UTF_8))
   }
 }
